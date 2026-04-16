c761cff8bac892f66d2f903965fb4191a59b09ed
Lena <lenazhu2007@gmail.com>
2026-04-16 11:30:33 -0400

    use nvidia beta package

--- hosts/celosia/nvidia.nix
+++ hosts/celosia/nvidia.nix
@@ -17,6 +17,6 @@
     open = true;
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 }
