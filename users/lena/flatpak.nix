c65d659c731ca6597bf11ba082938a6f5edbc302
Lena <lenazhu2007@gmail.com>
2026-04-16 11:31:20 -0400

    feat: flatpak (temp solution)

---
+++ users/lena/flatpak.nix
@@ -0,0 +1,16 @@
+{
+  username,
+  host-config,
+  pkgs,
+  lib,
+  ...
+}:
+lib.mkIf host-config.gui {
+  services.flatpak.enable = true;
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo";
+  };
+}
