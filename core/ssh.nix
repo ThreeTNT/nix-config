bb34225379beca8b790436e4caa982d3c9d22b5c
Lena <lenazhu2007@gmail.com>
2026-07-03 01:35:38 -0400

    fixes for minipc-1

--- core/ssh.nix
+++ core/ssh.nix
@@ -1,21 +1,19 @@
 { pkgs, lib, ... }:
 {
-  services.openssh = lib.mkMerge [
-    { enable = true; }
-    (
-      if pkgs.stdenv.isLinux then
-        {
-          PasswordAuthentication = true;
-          KexAlgorithms = [
-            "mlkem768x25519-sha256"
-            "sntrup761x25519-sha512"
-            "sntrup761x25519-sha512@openssh.com"
-            "curve25519-sha256"
-            "curve25519-sha256@libssh.org"
-          ];
-        }
-      else
-        { }
-    )
-  ];
+  services.openssh.enable = true;
+  services.openssh.settings =
+    if pkgs.stdenv.isLinux then
+      {
+        PasswordAuthentication = true;
+        KexAlgorithms = [
+          "mlkem768x25519-sha256"
+          "sntrup761x25519-sha512"
+          "sntrup761x25519-sha512@openssh.com"
+          "curve25519-sha256"
+          "curve25519-sha256@libssh.org"
+        ];
+      }
+    else
+      { };
+
 }
