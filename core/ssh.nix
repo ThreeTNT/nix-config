00d74dc4d139f7f375d0047b40f2182a35f10570
ThreeTNT <lenazhu2007@gmail.com>
2026-07-29 03:00:41 -0400

    fix openssh config for macos (again)

--- core/ssh.nix
+++ core/ssh.nix
@@ -1,19 +1,25 @@
 { pkgs, lib, ... }:
 {
-  services.openssh.enable = true;
-  services.openssh.settings =
-    if pkgs.stdenv.isLinux then
-      {
-        PasswordAuthentication = true;
-        KexAlgorithms = [
-          "mlkem768x25519-sha256"
-          "sntrup761x25519-sha512"
-          "sntrup761x25519-sha512@openssh.com"
-          "curve25519-sha256"
-          "curve25519-sha256@libssh.org"
-        ];
-      }
-    else
-      { };
-
+  services.openssh = lib.mkMerge [
+    (
+      if pkgs.stdenv.isLinux then
+        {
+          settings = {
+            PasswordAuthentication = true;
+            KexAlgorithms = [
+              "mlkem768x25519-sha256"
+              "sntrup761x25519-sha512"
+              "sntrup761x25519-sha512@openssh.com"
+              "curve25519-sha256"
+              "curve25519-sha256@libssh.org"
+            ];
+          };
+        }
+      else
+        { }
+    )
+    {
+      enable = true;
+    }
+  ];
 }
