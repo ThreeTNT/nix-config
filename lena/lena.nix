27a6d25a5e89e10155417f3fe29d92ae79074ea0
ThreeTNT <lenazhu2007@gmail.com>
2026-09-24 00:04:43 -0400

    add rui

--- lena/lena.nix
+++ lena/lena.nix
@@ -6,7 +6,8 @@
   users.users.lena = {
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICw7CHZkiNoG7StuF3NBnCHRWvsujJdX4ZTDIhz6YonH"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICw7CHZkiNoG7StuF3NBnCHRWvsujJdX4ZTDIhz6YonH" # Me
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAq4XsRorOLo7URBB1krIzhJi/ODqjUtD345D/DUrEkH" # Rui
     ];
   };
 
